0823449c182058102ad29f05b2e8f99a824a969b
poymanov <n.poymanov@gmail.com>
2019-03-20 22:18:02 +0300

    Методы для хранилища резюме

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,70 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size(), null);
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        int size = size();
+
+        storage[size] = resume;
     }
 
     Resume get(String uuid) {
+        for (Resume resume: getAll()) {
+            if (resume.uuid.contains(uuid)) {
+                return resume;
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+        int deletedIndex = 0;
+        boolean hasDeletedIndex = false;
+
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.contains(uuid)) {
+                deletedIndex = i;
+                hasDeletedIndex = true;
+                break;
+            }
+        }
+
+        if (!hasDeletedIndex) {
+            return;
+        }
+
+        storage[deletedIndex] = null;
+
+        for (int i = ++deletedIndex; i < 10000; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+
+            int newPosition = i;
+            newPosition--;
+
+            storage[newPosition] = storage[i];
+            storage[i] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+
+        return storage.length;
     }
 }
