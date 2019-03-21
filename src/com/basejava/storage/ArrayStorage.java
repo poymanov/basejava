81d835b79e795c1a2c01732d918c203b87d4832d
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-21 22:43:03 +0300

    Валидации для методов хранилища

--- src/com/basejava/storage/ArrayStorage.java
+++ src/com/basejava/storage/ArrayStorage.java
@@ -7,8 +7,22 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private int maxSize = 10000;
+    private Resume[] storage = new Resume[maxSize];
     private int size;
+
+    public void update(Resume resume) {
+        if (findResumeByUuid(resume.getUuid()) == null) {
+            printNotFound();
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(resume.getUuid())) {
+                storage[i] = resume;
+            }
+        }
+    }
 
     public void clear() {
         Arrays.fill(storage, 0, size(), null);
@@ -16,11 +30,69 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
+        if (findResumeByUuid(resume.getUuid()) != null) {
+            printDuplicate();
+            return;
+        }
+
+        int currentSize = size;
+
+        if (++currentSize > maxSize) {
+            printOverflowMaxSize();
+            return;
+        }
+
         storage[size] = resume;
         size++;
     }
 
     public Resume get(String uuid) {
+        Resume resume = findResumeByUuid(uuid);
+
+        if (resume == null) {
+            printNotFound();
+            return null;
+        }
+
+        return resume;
+    }
+
+    public void delete(String uuid) {
+        if (findResumeByUuid(uuid) == null) {
+            printNotFound();
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void printNotFound() {
+        System.out.println("Resume is not found");
+    }
+
+    private void printDuplicate() {
+        System.out.println("Resume already exists");
+    }
+
+    private void printOverflowMaxSize() {
+        System.out.println("Overflow the maximum storage size (" + maxSize + ")");
+    }
+
+    private Resume findResumeByUuid(String uuid) {
         for (Resume resume: getAll()) {
             if (resume.getUuid().equals(uuid)) {
                 return resume;
@@ -28,39 +100,5 @@ public class ArrayStorage {
         }
 
         return null;
-    }
-
-    public void delete(String uuid) {
-        int deletedIndex = -1;
-
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                deletedIndex = i;
-                break;
-            }
-        }
-
-        if (deletedIndex < 0) {
-            return;
-        }
-
-        storage[deletedIndex] = null;
-
-        for (int i = 0; i < size; i++) {
-            storage[i] = storage[i + 1];
-        }
-
-        size--;
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
     }
 }
