e8d8f271648b71f6959a0b081891e8b00fc6b943
Nicolay Poymanov <n.poymanov@gmail.com>
2019-03-28 10:51:53 +0300

    HW4: add method invoke

---
+++ src/com/basejava/MainReflection.java
@@ -0,0 +1,14 @@
+package com.basejava;
+
+import com.basejava.model.Resume;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = Resume.class.getDeclaredMethod("toString");
+        System.out.println(method.invoke(new Resume()));
+    }
+}
