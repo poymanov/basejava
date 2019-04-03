972b4a078d07f8b75d7f9093bee168613f7db3b7
poymanov <n.poymanov@gmail.com>
2019-04-03 22:40:05 +0300

    HW6: add all storage test

---
+++ test/com/basejava/storage/AllStorageTest.java
@@ -0,0 +1,18 @@
+package com.basejava.storage;
+
+import org.junit.jupiter.api.Nested;
+
+public class AllStorageTest {
+
+    @Nested
+    class Array extends ArrayStorageTest {}
+
+    @Nested
+    class SortedArray extends SortedArrayStorageTest {}
+
+    @Nested
+    class List extends ListStorageTest {}
+
+    @Nested
+    class Map extends MapStorageTest {}
+}
