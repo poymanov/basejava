8d7ae2e3fc519c59322a840e1c7b5c6d01ce259d
poymanov <n.poymanov@gmail.com>
2019-04-22 23:30:25 +0300

    HW8: refactor MainTestClass

--- src/com/basejava/MainTestFile.java
+++ src/com/basejava/MainTestFile.java
@@ -3,38 +3,27 @@ package com.basejava;
 import java.io.File;
 
 public class MainTestFile {
+
     public static void main(String[] args) {
-        String root = "./";
-        printData(root, 0);
+        File file = new File("./");
+        printData(file);
     }
 
-    private static void printData(String path, int depth) {
-        File file = new File(path);
-
+    private static void printData(File file) {
         String[] list = file.list();
 
         if (list != null) {
             for (String item : list) {
-
-                if (item.equals(".idea") || item.equals(".git")) {
-                    continue;
-                }
-
-                for (int i = 0; i < depth; i++) {
-                    System.out.print('-');
-                }
-
                 System.out.println(item);
 
-                File file2 = new File(path + "/" + item);
+                File file2 = new File(file.getPath() + "/" + item);
 
                 if (file2.isDirectory()) {
-                    printData(file2.getPath(), ++depth);
+                    printData(file2);
                 }
             }
         } else {
             System.out.println(file.getName());
         }
-
     }
 }
