844b6aff4dc38ddf4e378f0dc18130f6eed8f77e
Nicolay Poymanov <n.poymanov@gmail.com>
2019-04-24 21:49:28 +0300

    HW9: print project files structure with indents

--- src/com/basejava/MainTestFile.java
+++ src/com/basejava/MainTestFile.java
@@ -6,22 +6,25 @@ public class MainTestFile {
 
     public static void main(String[] args) {
         File file = new File("./");
-        printData(file);
+        printData(file, 0);
     }
 
-    private static void printData(File root) {
+    private static void printData(File root, int depth) {
         File[] files = root.listFiles();
+        String indent = new String(new char[depth]).replace("\0", " ");
+        depth++;
 
         if (files != null) {
             for (File file : files) {
                 if (file.isDirectory()) {
-                    printData(file);
+                    System.out.println(indent + file.getName());
+                    printData(file, depth);
                 } else {
-                    System.out.println(file.getName());
+                    System.out.println(indent + file.getName());
                 }
             }
         } else {
-            System.out.println(root.getName());
+            System.out.println(indent + root.getName());
         }
     }
 }
