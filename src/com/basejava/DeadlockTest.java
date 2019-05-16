2948e2190e23946f9c8ee519e9420fc53e6bc9dd
Poymanov <n.poymanov@gmail.com>
2019-05-16 09:57:05 +0300

    HW11: add deadlock case

---
+++ src/com/basejava/DeadlockTest.java
@@ -0,0 +1,52 @@
+package com.basejava;
+
+public class DeadlockTest {
+    private static final Object Lock1 = new Object();
+    private static final Object Lock2 = new Object();
+
+    public static void main(String[] args) {
+        ThreadDemo1 t1 = new ThreadDemo1();
+        ThreadDemo2 t2 = new ThreadDemo2();
+        t1.start();
+        t2.start();
+    }
+
+    private static class ThreadDemo1 extends Thread {
+        public void run() {
+            synchronized (Lock1) {
+                System.out.println("Thread 1: Holding lock 1...");
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Thread 1: Waiting for lock 2...");
+
+                synchronized (Lock2) {
+                    System.out.println("Thread 1: Holding lock 1 & 2...");
+                }
+            }
+        }
+    }
+    private static class ThreadDemo2 extends Thread {
+        public void run() {
+            synchronized (Lock2) {
+                System.out.println("Thread 2: Holding lock 2...");
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Thread 2: Waiting for lock 1...");
+
+                synchronized (Lock1) {
+                    System.out.println("Thread 2: Holding lock 1 & 2...");
+                }
+            }
+        }
+    }
+}
